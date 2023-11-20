5c0b66dfef793977788db8ed47217efd2d69e712
Joakim Kilby <jockekilby@gmail.com>
2023-11-20 16:02:29 +0100

    add do-nothing overlay to force high tile-depth

--- modules/globebrowsing/src/layer.cpp
+++ modules/globebrowsing/src/layer.cpp
@@ -126,7 +126,8 @@ namespace {
             "SingleImageProvider", "ImageSequenceTileProvider",
             "SizeReferenceTileProvider", "TemporalTileProvider", "TileIndexTileProvider",
             "TileProviderByIndex", "TileProviderByLevel", "SolidColor",
-            "SpoutImageProvider", "VideoTileProvider", "PlanetaryTrailTileProvider")]];
+            "SpoutImageProvider", "VideoTileProvider", "PlanetaryTrailTileProvider",
+            "ForceHighResolutionTileProvider")]];
 
         // Determine whether the layer is enabled or not. If this value is not specified,
         // the layer is disabled
@@ -316,6 +317,7 @@ Layer::Layer(layers::Group::ID id, const ghoul::Dictionary& layerDict, LayerGrou
             case layers::Layer::ID::TileProviderByLevel:
             case layers::Layer::ID::VideoTileProvider:
             case layers::Layer::ID::PlanetaryTrailTileProvider:
+            case layers::Layer::ID::ForceHighResolutionTileProvider:
                 if (_tileProvider) {
                     removePropertySubOwner(*_tileProvider);
                 }
@@ -476,6 +478,7 @@ void Layer::initializeBasedOnType(layers::Layer::ID id, ghoul::Dictionary initDi
         case layers::Layer::ID::TileProviderByLevel:
         case layers::Layer::ID::VideoTileProvider:
         case layers::Layer::ID::PlanetaryTrailTileProvider:
+        case layers::Layer::ID::ForceHighResolutionTileProvider:
             // We add the id to the dictionary since it needs to be known by
             // the tile provider
             initDict.setValue(
@@ -510,6 +513,7 @@ void Layer::addVisibleProperties() {
         case layers::Layer::ID::TileProviderByLevel:
         case layers::Layer::ID::VideoTileProvider:
         case layers::Layer::ID::PlanetaryTrailTileProvider:
+        case layers::Layer::ID::ForceHighResolutionTileProvider:
             if (_tileProvider) {
                 addPropertySubOwner(*_tileProvider);
             }
